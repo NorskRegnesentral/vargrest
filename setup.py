597548465a17f55a6c31d88313e95f02100e9130
Vegard Berg Kvernelv <vegard@nr.no>
2020-06-30 09:17:27 +0200

    Updated setup.py to better suit pypi. Updated version number.

--- setup.py
+++ setup.py
@@ -3,8 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='vargrest',
     version=open('vargrest/VERSION.txt').read(),
-    description='Package',
+    description='Variogram estimation for ResQml models converted by nrresqml',
     author='Norwegian Computing Center',
+    license='GPL-3.0',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/NorskRegnesentral/vargrest',
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'vargrest': ['VERSION.txt']
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
 )
